61330674b4290d99f32fc13c7907cfbd2cca6f8d
Kemal Hadimli <disq@users.noreply.github.com>
2022-11-11 15:49:22 +0000

    RGBW leds, new white effect, effect mode

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 #include "PicoLed.hpp"
 
 // Adjust according to the number of LEDs you have
-const uint NUM_LEDS = 3;
+const uint NUM_LEDS = 151;
 
 // Defaults... between 0 and 1.0f
 // The speed that the LEDs will start cycling at
@@ -51,10 +51,11 @@ const float_t BRIGHTNESS_SCALE = 255;
 bool cycle;
 uint32_t encoder_last_blink = 0;
 bool encoder_blink_state = false;
+uint8_t effect_mode;
 
 using namespace pimoroni;
 
-auto led_strip = PicoLed::addLeds<PicoLed::WS2812B>(pio0, 0, LED_DATA_PIN, NUM_LEDS, PicoLed::FORMAT_GRB);
+auto led_strip = PicoLed::addLeds<PicoLed::WS2812B>(pio0, 0, LED_DATA_PIN, NUM_LEDS, PicoLed::FORMAT_WRGB);
 
 Button button_a(BUTTON_A_PIN, Polarity::ACTIVE_LOW, 0);
 Button button_b(BUTTON_B_PIN, Polarity::ACTIVE_LOW, 0);
@@ -68,8 +69,20 @@ enum ENCODER_MODE {
     COLOUR,
     ANGLE,
     BRIGHTNESS,
-    SPEED
+    SPEED,
+    EFFECT,
+
+    MODE_COUNT
 };
+
+enum EFFECT_MODE {
+    HUE_CYCLE,
+    WHITE_CHASE,
+
+    EFFECT_COUNT,
+};
+
+const enum EFFECT_MODE DEFAULT_EFFECT = HUE_CYCLE;
 
 float wrap(float v, float min, float max) {
   if(v <= min)
@@ -82,7 +95,7 @@ float wrap(float v, float min, float max) {
 }
 
 
-void colour_cycle(float hue, float t, float angle) {
+void effect_cycle(float hue, float t, float angle) {
   auto hue_deg = hue * 360.0f;
   auto angle_deg = angle * 360.0f;
 
@@ -92,7 +105,16 @@ void colour_cycle(float hue, float t, float angle) {
     float percent_along = (float)i / led_strip.getNumLeds();
     float offset = sinf((percent_along + 0.5f + t) * M_PI) * angle_deg;
     float h = wrap((hue_deg + offset) / 360.0f, 0.0f, 1.0f);
-    led_strip.setPixelColor(i, PicoLed::HSV(h*255.0f, 255.0f, 255.0f));
+
+    switch(effect_mode) {
+      case EFFECT_MODE::HUE_CYCLE:
+      default:
+        led_strip.setPixelColor(i, PicoLed::HSV(uint8_t(h*255.0f), 255.0f, 255.0f));
+        break;
+      case EFFECT_MODE::WHITE_CHASE:
+        led_strip.setPixelColor(i, PicoLed::RGBW(0, 0, 0, uint8_t((1.0f - h)*255.0f)));
+        break;
+    }
   }
   led_strip.show();
 }
@@ -108,19 +130,38 @@ void set_cycle(bool v) {
 }
 
 uint32_t encoder_colour_by_mode(ENCODER_MODE mode) {
+  uint32_t col;
   switch(mode) {
     case ENCODER_MODE::OFF:
     default:
-      return 0;
+      col = 0;
+      break;
     case ENCODER_MODE::COLOUR:
-      return 0xFFFF00; // yellow
+      col = 0xFFFF00; // yellow
+      break;
     case ENCODER_MODE::ANGLE:
-      return 0xFF8000; // orange
+      col = 0xFF8000; // orange
+      break;
     case ENCODER_MODE::BRIGHTNESS:
-      return 0xFFFFFF; // white
+      col = 0xFFFFFF; // white
+      break;
     case ENCODER_MODE::SPEED:
-      return 0xFF0000; // red
+      col = 0xFF0000; // red
+      break;
+    case ENCODER_MODE::EFFECT:
+      col = 0xFF00FF; // purple
+      break;
   }
+  uint8_t col_r = (col >> 16) & 0xFF;
+  uint8_t col_g = (col >> 8) & 0xFF;
+  uint8_t col_b = col & 0xFF;
+  if (encoder_blink_state) {
+    col_r = col_r >> 1;
+    col_g = col_g >> 1;
+    col_b = col_b >> 1;
+  }
+  enc.set_led(col_r, col_g, col_b);
+  return col;
 }
 
 void encoder_loop(ENCODER_MODE mode) {
@@ -129,41 +170,13 @@ void encoder_loop(ENCODER_MODE mode) {
   if (encoder_last_blink == 0 || (encoder_last_blink + encoder_blink_interval)<ts) {
     encoder_last_blink = ts;
     encoder_blink_state = !encoder_blink_state;
-    auto col = encoder_colour_by_mode(mode);
-    uint8_t col_r = (col >> 16) & 0xFF;
-    uint8_t col_g = (col >> 8) & 0xFF;
-    uint8_t col_b = col & 0xFF;
-    if (encoder_blink_state) {
-      col_r = col_r >> 1;
-      col_g = col_g >> 1;
-      col_b = col_b >> 1;
-    }
-    enc.set_led(col_r, col_g, col_b);
+    encoder_colour_by_mode(mode);
   }
 }
 
 ENCODER_MODE encoder_state_by_mode(ENCODER_MODE mode) {
-  switch(mode) {
-    case ENCODER_MODE::OFF:
-      enc.set_led(0, 0, 0);
-      break;
-    case ENCODER_MODE::COLOUR:
-      enc.set_brightness(ENC_DEFAULT_BRIGHTNESS);
-      enc.set_led(255, 255, 0); // yellow
-      break;
-    case ENCODER_MODE::ANGLE:
-      enc.set_brightness(ENC_DEFAULT_BRIGHTNESS);
-      enc.set_led(255, 128, 0); // orange
-      break;
-    case ENCODER_MODE::BRIGHTNESS:
-      enc.set_brightness(brightness);
-      enc.set_led(255, 255, 255); // white
-      break;
-    case ENCODER_MODE::SPEED:
-      enc.set_brightness(ENC_DEFAULT_BRIGHTNESS);
-      enc.set_led(255, 0, 0); // red
-      break;
-  }
+  enc.set_brightness(mode == ENCODER_MODE::BRIGHTNESS ? brightness : ENC_DEFAULT_BRIGHTNESS);
+  encoder_colour_by_mode(mode);
   return mode;
 }
 
@@ -187,6 +200,7 @@ int main() {
   float_t speed = DEFAULT_SPEED;
   float_t hue = DEFAULT_HUE;
   float_t angle = DEFAULT_ANGLE;
+  effect_mode = DEFAULT_EFFECT;
 
   set_cycle(true);
 
@@ -216,8 +230,8 @@ int main() {
           case ENCODER_MODE::COLOUR:
             hue += count;
             hue = wrap(hue, 0.0f, 1.0f);
-            colour_cycle(hue, 0, angle);
             printf("new hue start angle: %f\n", hue);
+            effect_cycle(hue, 0, angle);
             set_cycle(false);
             break;
 
@@ -225,15 +239,15 @@ int main() {
             angle += count;
             angle = std::min(1.0f, std::max(0.0f, angle));
             printf("new hue end angle: %f\n", angle);
-            colour_cycle(hue, 0, angle);
+            effect_cycle(hue, 0, angle);
             set_cycle(false);
             break;
 
           case ENCODER_MODE::BRIGHTNESS:
             brightness += count;
             brightness = std::min(1.0f, std::max(MIN_BRIGHTNESS, brightness));
-            led_strip.setBrightness((uint8_t)(brightness*BRIGHTNESS_SCALE));
             printf("new brightness: %f\n", brightness);
+            led_strip.setBrightness((uint8_t)(brightness*BRIGHTNESS_SCALE));
             enc.set_brightness(brightness);
             set_cycle(true);
             break;
@@ -243,6 +257,13 @@ int main() {
             speed = std::min(1.0f, std::max(0.01f, speed));
             printf("new speed: %f\n", speed);
             set_cycle(true);
+            break;
+
+          case ENCODER_MODE::EFFECT:
+            effect_mode = (EFFECT_MODE)(((int)effect_mode + 1) % (int)EFFECT_MODE::EFFECT_COUNT);
+            printf("new effect: %d\n", effect_mode);
+            effect_cycle(hue, 0, angle);
+            set_cycle(false);
             break;
         }
       }
@@ -257,6 +278,8 @@ int main() {
       hue = DEFAULT_HUE;
       angle = DEFAULT_ANGLE;
       brightness = DEFAULT_BRIGHTNESS;
+      effect_mode = DEFAULT_EFFECT;
+      led_strip.setBrightness((uint8_t)(brightness*BRIGHTNESS_SCALE));
       mode = encoder_state_by_mode(ENCODER_MODE::OFF);
       set_cycle(true);
     }
@@ -268,37 +291,14 @@ int main() {
     }
 
     if (a_pressed) {
-      switch(mode) {
-        case ENCODER_MODE::OFF:
-          mode = encoder_state_by_mode(ENCODER_MODE::COLOUR);
-          printf("[mode] start colour\n");
-          break;
-
-        case ENCODER_MODE::COLOUR:
-          mode = encoder_state_by_mode(ENCODER_MODE::ANGLE);
-          printf("[mode] end colour\n");
-          break;
-
-        case ENCODER_MODE::ANGLE:
-          mode = encoder_state_by_mode(ENCODER_MODE::BRIGHTNESS);
-          printf("[mode] brightness\n");
-          break;
-
-        case ENCODER_MODE::BRIGHTNESS:
-          mode = encoder_state_by_mode(ENCODER_MODE::SPEED);
-          printf("[mode] speed\n");
-          break;
-
-        case ENCODER_MODE::SPEED:
-          mode = encoder_state_by_mode(ENCODER_MODE::OFF);
-          printf("[mode] off\n");
-          break;
-      }
+      mode = (ENCODER_MODE)(((int)mode + 1) % (int)ENCODER_MODE::MODE_COUNT);
+      printf("[mode] new mode: %d\n", mode);
+      encoder_state_by_mode(mode);
     }
 
     if(cycle) {
 //      printf("[cycle] hue: %f, angle: %f, speed: %f, brightness: %f\n", hue, angle, speed, brightness);
-      colour_cycle(hue, (float)t * speed, angle);
+      effect_cycle(hue, (float) t * speed, angle);
     }
     if (!cycle && mode != ENCODER_MODE::OFF) encoder_loop(mode);
 
